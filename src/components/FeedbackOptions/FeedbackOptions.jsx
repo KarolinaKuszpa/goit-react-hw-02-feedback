bff471b5320ecff61af720623c19c64d84ad8f51
KarolinaKuszpa <117316629+KarolinaKuszpa@users.noreply.github.com>
2023-06-16 20:36:59 +0200

    Feedback

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -2,12 +2,25 @@ import React from 'react';
 import styles from './FeedbackOptions.module.css';
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  const getButtonStyle = option => {
+    switch (option) {
+      case 'good':
+        return styles.buttonGood;
+      case 'neutral':
+        return styles.buttonNeutral;
+      case 'bad':
+        return styles.buttonBad;
+      default:
+        return styles.button;
+    }
+  };
+
   return (
     <div className={styles.container}>
       {options.map(option => (
         <button
           key={option}
-          className={styles.button}
+          className={`${styles.button} ${getButtonStyle(option)}`}
           onClick={() => onLeaveFeedback(option)}
         >
           {option}
